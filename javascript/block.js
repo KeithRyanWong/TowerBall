12c3448dc1d4132da73469b5bcf23388ced04a87
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-03 09:50:03 -0700

    Revert to 4f710a2f0

--- javascript/block.js
+++ javascript/block.js
@@ -1,19 +1,10 @@
 import * as Util from './utils.js';
-import { Engine, Render, World, Bodies, Vector, Vertices } from 'matter-js';
+import { Engine, Render, World, Bodies } from 'matter-js';
 
 
 class Block {
   constructor(x, y, w, h, world) {
-    this.options = {
-      friction: 1,
-      restitution: 0,
-      frictionStatic: 10,
-      render: { 
-        fillStyle: Util.randColor(),
-        lineWidth: 1
-      }
-    };
-    this.body = Bodies.rectangle(x, y, w, h, this.options);
+    this.body = Bodies.rectangle(x, y, w, h);
     this.w = w;
     this.h = h;
     World.add(world, [this.body]);
@@ -21,30 +12,17 @@ class Block {
     this.area = w * h;
   }
 
-  canBeDestroyed(w, h) {
-    let {x, y} = this.body.position;
-    return y > h;
-  }
-
-  occupies(x, y) {
-    let pos = Vector.create(x, y);
-    return Vertices.contains(this.body.vertices, pos);
-  }
 
   draw(ctx) {
     let {x, y} = this.body.position;
-
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#000000';
-
-    let vertices = this.body.vertices;
-    ctx.moveTo(vertices[0].x, vertices[0].y);
-    for (let i = 1; i < vertices.length; i++) {
-        ctx.lineTo(vertices[i].x, vertices[i].y);
-    }
-    ctx.lineTo(vertices[0].x, vertices[0].y);
-
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + this.w , y);
+    ctx.lineTo(x + this.w, y + this.h);
+    ctx.lineTo(x, y + this.h);
+    ctx.lineTo(x, y);
     ctx.stroke();
     ctx.fillStyle = this.color;
     ctx.fill();
